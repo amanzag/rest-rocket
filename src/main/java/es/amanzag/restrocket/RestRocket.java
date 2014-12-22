e6ffb1078a4824559f0b652533da65e0acce516f
Alberto Manzaneque <amanzag@gmail.com>
2014-12-22 17:40:44 +0100

    Initial configuration of maven project and grizzly server

---
+++ src/main/java/es/amanzag/restrocket/RestRocket.java
@@ -0,0 +1,40 @@
+package es.amanzag.restrocket;
+
+import java.io.IOException;
+
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.grizzly.http.util.HttpStatus;
+
+/**
+ * @author amanzaneque
+ *
+ */
+public class RestRocket {
+
+    /**
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        final HttpServer server = HttpServer.createSimpleServer();
+        
+        server.getServerConfiguration().addHttpHandler(new HttpHandler() {
+            @Override
+            public void service(Request request, Response response) throws Exception {
+                response.setStatus(HttpStatus.OK_200);
+            }
+        }, "/rocket");
+        
+        server.start();
+        
+        System.out.println("Press any key to exit");
+        System.in.read();
+        
+        server.shutdown();
+    }
+
+}
