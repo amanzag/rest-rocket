adf3ae1b13cd66c9d0ce61a7882571be55d6b352
Alberto Manzaneque <amanzag@gmail.com>
2015-08-04 12:38:14 +0200

    using relative URLs for the api

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,14 +3,14 @@ var rocketLauncherApp = angular.module('rocketLauncherApp', []);
 rocketLauncherApp.controller('RocketController', function ($scope, $http, $interval) {
 	
 	$scope.sendCommand = function(command) {
-		$http.post('/api/rocket/' + command);
+		$http.post('api/rocket/' + command);
 	}
 	$scope.showCamera = false;
 	
-	$http.head('/api/camera/frame').success(function() {
+	$http.head('api/camera/frame').success(function() {
 		$interval(function() {
 			$scope.showCamera = true;
-			$scope.cameraUrl = '/api/camera/frame?' + Math.random();
+			$scope.cameraUrl = 'api/camera/frame?' + Math.random();
 		}, 2000);
 	});
 	
